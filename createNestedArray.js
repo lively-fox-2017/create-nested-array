7b635a4f29edec702a7b7a1234679af85714705f
ipunganwar <ipung.anwar1@gmail.com>
2017-09-20 16:42:15 +0700

    release 0

--- createNestedArray.js
+++ createNestedArray.js
@@ -1,29 +1,21 @@
 'use strict';
 
-var arr = [];
+function createNestedArr(jumlahRow, jumlahCol){
+	let x = 'abcdefghijklmnopqrstuvwxyz';
+	
+	let row = [];
 
-function createObj(name, phase, gender){
-	var obj = {
-		'name' : name,
-		'phase' : phase,
-		'gender' : gender
-	}
-
-	return arr.push(obj);
-
-}
-
-createObj('Akbar', 1, 'male')
-createObj('Icha', 1, 'female')
-
-console.log(arr)
-
-function getData(name){
-	for(var i = 0; i <  arr.length; i++){
-		if(arr[i].name === name){
-			return arr[i]
+	for(let i = 0; i < jumlahRow; i++){
+		let kolom = [];
+		for(let j = 0; j < jumlahCol; j++){
+			kolom.push(x[Math.floor((Math.random() * x.length))])
 		}
+		row.push(kolom)
 	}
+
+	return row;
+	
 }
 
-console.log(getData('Icha'))
+console.log(createNestedArr(5, 3))
+console.log(createNestedArr(4, 4))
